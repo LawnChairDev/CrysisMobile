72784f56d83a7ef2451997eaa9366661c1b695bd
Matthew Terranova <MattTerranova92@gmail.com>
2016-08-06 15:45:58 -0700

    [SETUP] Login Component

--- crysis/index.ios.js
+++ crysis/index.ios.js
@@ -8,6 +8,8 @@ import {
   View
 } from 'react-native';
 
+import Login from './ios/src/Components/Login'
+
 class crysis extends Component {
 
   handleRender(route, navigator) {
---
+++ crysis/ios/src/Components/Login.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+import {
+	View,
+	Text,
+  StyleSheet,
+	TouchableHighlight,
+  TextInput
+} from 'react-native';
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      usernameTextbox: '',
+      passwordTextbox: ''
+    }
+  }
+	navigate(routeName) {
+		this.props.navigator.push({
+			name: routeName
+		});
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text style={styles.title}>Crysis</Text>
+          <TextInput
+            style={styles.textBoxes}
+            onChangeText={(usernameTextbox) => this.setState({usernameTextbox})}
+            returnKeyType={'next'}
+            placeholder={'Username'}
+            value={this.state.usernameTextbox}
+          />
+          <TextInput
+            style={styles.textBoxes}
+            onChangeText={(passwordTextbox) => this.setState({passwordTextbox})}
+            placeholder={'Password'}
+            secureTextEntry={true}
+            onSubmitEditing={this.navigate.bind(this, 'Home')}
+            value={this.state.passwordTextbox}
+          />
+      		<TouchableHighlight
+      			onPress={ this.navigate.bind(this, 'Home') }
+      			style={styles.button}>
+        			<Text style={styles.buttonText}>Login</Text>
+      		</TouchableHighlight>
+			</View>
+		)
+	}
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#BFDBF3',
+    padding: 10,
+  },
+  button: {
+    height: 50,
+    backgroundColor: '#CE0536',
+    alignSelf: 'stretch',
+    alignItems: 'center',
+    marginTop: 10,
+    justifyContent: 'center'
+  },
+  buttonText: {
+    fontSize: 22,
+    color: '#FFF',
+    alignSelf: 'center'
+  },
+  title: {
+    fontSize: 50,
+    marginBottom: 15
+  },
+  textBoxes: {
+    height: 40,
+    margin: 5,
+    padding: 10,
+    borderColor: 'gray',
+    borderWidth: 1
+  }
+});
+
+export default Login;
